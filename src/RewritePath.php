fc09b22824a6f6339e4f17a4af7d4e4684f26719
Vitaly Egorov <egorov@samsonos.com>
2014-12-05 13:01:40 +0200

    Added resource rewriter class

---
+++ src/RewritePath.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: egorov
+ * Date: 05.12.2014
+ * Time: 12:36
+ */
+namespace samson\html;
+
+/**
+ * Resource paths rebuilder
+ * @package samson\html
+ */
+class RewritePath
+{
+    /** @var string Source file path */
+    protected $source;
+
+    /** @var string Resource path matching template */
+    protected $template;
+
+    /** @var array Collection of paths for rewriting */
+    protected $found;
+
+    /**
+     * Constructor
+     * @param string $source File path
+     * @param string Resource path matching template
+     */
+    public function __construct($source, $template)
+    {
+        $this->source = $source;
+        $this->template = $template;
+    }
+
+    
+}
